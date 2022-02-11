58c8ac82d8c65dfe3754fbce133f483bf49c1873
guideofandy <anderson.armeya@gmail.com>
2022-02-11 11:01:36 -0400

    HomePage & _app with layouts

---
+++ components/layouts/slidebar.js/index.js
@@ -0,0 +1,10 @@
+import styles from "../../../styles/Layouts.module.css";
+
+const Slidebar = () => {
+  return (
+    <aside className={styles.sidebar}>
+
+    </aside>);
+};
+
+export default Slidebar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import Layouts from '../components/layouts'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layouts>
+      <Component {...pageProps} />
+    </Layouts>
+  )
 }
 
 export default MyApp
