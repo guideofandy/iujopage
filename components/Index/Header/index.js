008bb47a66e6f9001b15a93a9f7c4609ea16d823
guideofandy <anderson.armeya@gmail.com>
2022-02-13 20:06:32 -0400

    Solving erros with deployd #2

--- components/Index/Header/index.js
+++ components/Index/Header/index.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.headerImage}>
-        <Link>
+        <Link href="/">
           <Image src={'/img/principal.png'} width={833} height={261} alt={"Logo de La institución"} />
         </Link>
       </div>
