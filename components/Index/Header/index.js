e7fa31edf4e19b947f34af5cf0cb91fc7534a03e
guideofandy <anderson.armeya@gmail.com>
2022-02-13 20:10:16 -0400

    Solvin errors with deployd #3

--- components/Index/Header/index.js
+++ components/Index/Header/index.js
@@ -1,15 +1,12 @@
 import Image from 'next/image';
 import styles from '../../../styles/Index.module.css'
 import { AiOutlineMenu } from "react-icons/ai";
-import Link from 'next/link';
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.headerImage}>
-        <Link href="/">
           <Image src={'/img/principal.png'} width={833} height={261} alt={"Logo de La institución"} />
-        </Link>
       </div>
       <div className={styles.Links}>
         <a>SIGEA</a>
